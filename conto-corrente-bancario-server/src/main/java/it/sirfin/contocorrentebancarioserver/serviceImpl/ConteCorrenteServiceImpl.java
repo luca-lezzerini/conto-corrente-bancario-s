16b23a16c5d96563ecd6744ab60697c9f7fb7236
daniele-bit <daniele.fas95@gmail.com>
2021-03-11 12:47:13 +0100

    Risolto creare service e serviceImpl vuoti #24

---
+++ conto-corrente-bancario-server/src/main/java/it/sirfin/contocorrentebancarioserver/serviceImpl/ConteCorrenteServiceImpl.java
@@ -0,0 +1,9 @@
+package it.sirfin.contocorrentebancarioserver.serviceImpl;
+
+import it.sirfin.contocorrentebancarioserver.service.ContoCorrenteSevice;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConteCorrenteServiceImpl implements ContoCorrenteSevice {
+
+}
