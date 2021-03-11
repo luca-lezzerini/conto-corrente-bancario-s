023443a2bcbe9460dad22950b23c2d0b1b7232a4
capponiFelice92 <78529321+capponiFelice92@users.noreply.github.com>
2021-03-11 12:17:14 +0100

    Creare server vuoto #1

---
+++ conto-corrente-bancario-server/src/test/java/it/sirfin/contocorrentebancarioserver/ContoCorrenteBancarioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.contocorrentebancarioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContoCorrenteBancarioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
