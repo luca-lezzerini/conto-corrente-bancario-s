dd416ec338a2b6cb9dbd8b0f5b3771c19dc98b3b
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-03-11 20:29:34 +0100

    risolto Creare home page #27

--- conto-corrente-bancario-s-client/src/app/app-routing.module.ts
+++ conto-corrente-bancario-s-client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GestioneCcComponent } from './gestione-cc/gestione-cc.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { InserisciComponent } from './inserisci/inserisci.component';
 
 const routes: Routes = [
   { path: 'app-inserisci', component: InserisciComponent },
-  { path: 'app-home-page', component: HomePageComponent }
-
+  { path: 'app-home-page', component: HomePageComponent },
+  { path: 'app-gestione-cc', component: GestioneCcComponent },
+  { path: '', redirectTo: '/app-home-page', pathMatch: 'full' }
 ];
 
 @NgModule({
