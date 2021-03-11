3b3d7fdd01049dd1bc84729d7ce1376d8cb2ff3c
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-11 16:30:17 +0100

    update CC

---
+++ conto-corrente-bancario-s-client/src/app/gestione-cc/cc.ts
@@ -0,0 +1,4 @@
+export class ContoCorrente {
+    id = null;
+    numConto = "";
+}
--- conto-corrente-bancario-s-client/src/app/gestione-cc/gestione-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/gestione-cc/gestione-cc.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { ContoCorrente } from './cc';
 
 @Component({
   selector: 'app-gestione-cc',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestione-cc.component.css']
 })
 export class GestioneCcComponent implements OnInit {
+  cc: ContoCorrente[] = [];
+  conto = new ContoCorrente();
 
   constructor(private route: Route, private http: HttpClient) { }
   // showForm = false;
