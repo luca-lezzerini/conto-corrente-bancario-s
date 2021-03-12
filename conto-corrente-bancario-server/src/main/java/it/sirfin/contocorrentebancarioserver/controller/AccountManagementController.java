108a40ae87f8497907f2bf00425344f33067bd6e
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-12 11:51:07 +0100

    Update AccountManagementController.java

--- conto-corrente-bancario-server/src/main/java/it/sirfin/contocorrentebancarioserver/controller/AccountManagementController.java
+++ conto-corrente-bancario-server/src/main/java/it/sirfin/contocorrentebancarioserver/controller/AccountManagementController.java
@@ -1,14 +1,26 @@
 package it.sirfin.contocorrentebancarioserver.controller;
 
+import it.sirfin.contocorrentebancarioserver.dto.ContoCorrenteDto;
+import it.sirfin.contocorrentebancarioserver.dto.ListaContiCorrenteDto;
 import it.sirfin.contocorrentebancarioserver.service.AccountManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
 @RestController
 public class AccountManagementController {
-    
+
     @Autowired
     AccountManagementService accountManagementService;
+
+    @RequestMapping("/aggiungi-cc")
+    @ResponseBody
+    public ListaContiCorrenteDto aggiungiCc(@RequestBody ContoCorrenteDto dto) {
+        //return accountManagementService.aggiungiCc(dto.getContoCorrente());
+        throw new UnsupportedOperationException();
+    }
 }
