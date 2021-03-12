424d9263111efbeaf5aa4ed541112566ec02ac7d
leoIapp <leoiappelli01@gmail.com>
2021-03-12 11:56:56 +0100

    Revert "Creare home page #27"
    This reverts commit 8798c485f93ada31f018fdeebc151082530178fc.

--- conto-corrente-bancario-s-client/src/app/app.module.ts
+++ conto-corrente-bancario-s-client/src/app/app.module.ts
@@ -7,25 +7,13 @@ import { AppComponent } from './app.component';
 import { GestioneCcComponent } from './gestione-cc/gestione-cc.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { GestioneClientiComponent } from './gestione-clienti/gestione-clienti.component';
-import { GestioneCpComponent } from './gestione-cp/gestione-cp.component';
-import { GestioneCdComponent } from './gestione-cd/gestione-cd.component';
-import { MostraTuttiIContiComponent } from './mostra-tutti-i-conti/mostra-tutti-i-conti.component';
-import { AssociaCcComponent } from './associa-cc/associa-cc.component';
-import { AssociaCpComponent } from './associa-cp/associa-cp.component';
-import { AssociaCdComponent } from './associa-cd/associa-cd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GestioneCcComponent,
     HomePageComponent,
-    GestioneClientiComponent,
-    GestioneCpComponent,
-    GestioneCdComponent,
-    MostraTuttiIContiComponent,
-    AssociaCcComponent,
-    AssociaCpComponent,
-    AssociaCdComponent
+    GestioneClientiComponent
   ],
   imports: [
     BrowserModule,
