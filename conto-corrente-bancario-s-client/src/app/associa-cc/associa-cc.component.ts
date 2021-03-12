f35b3b5558e9f081e061bcb43ce8c2fcf6102de9
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-12 17:10:03 +0100

    Update lato client Associa-cc

--- conto-corrente-bancario-s-client/src/app/associa-cc/associa-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/associa-cc/associa-cc.component.ts
@@ -1,6 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ContoCorrente } from '../conto-corrente';
+import { ContoCorrenteDto } from '../conto-corrente-dto';
+import { ListaClientiDto } from '../lista-clienti-dto';
+import { ListaContiCorrenteDto } from '../lista-conti-corrente-dto';
+import { RicercaClienteDto } from '../ricerca-cliente-dto';
+import { RicercaContoCorrenteDto } from '../ricerca-conto-corrente-dto';
 
 @Component({
   selector: 'app-associa-cc',
@@ -11,19 +17,27 @@ export class AssociaCcComponent implements OnInit {
 
   clienti: Cliente[] = [];
   cliente = new Cliente();
+  contoCorrente = new ContoCorrente();
   contiCorrente: ContoCorrente[] = [];
-  numeroConto = "";
-  constructor() { }
-
+  ricercaConto = "";
   ricercaCliente = "";
+  constructor(private http: HttpClient) { }
+
+
   ngOnInit(): void {
   }
 
   cercaCliente() {
-
+    let dto = new RicercaClienteDto();
+    dto.ricercaPerCognome = this.ricercaCliente;
+    this.http.post<ListaClientiDto>("http://localhost:8080/ricerca-c", dto)
+      .subscribe(r => this.clienti = r.listaClienti);
   }
 
   cercaContoCorrente() {
-
+    let dto = new RicercaContoCorrenteDto();
+    dto.codiceEsatto = this.ricercaConto;
+    this.http.post<ListaContiCorrenteDto>("http://localhost:8080/ricerca-cc", dto)
+      .subscribe(r => this.contiCorrente = r.listaContiCorrente);
   }
 }
