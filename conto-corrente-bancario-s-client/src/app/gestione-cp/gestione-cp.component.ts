c982499defa2c1eca7a5e0fcbc1ece66d307ea1d
ManuelBava <manuelbava1996@gmail.com>
2021-03-12 15:34:08 +0100

    implementato scheletro gestione cp client

--- conto-corrente-bancario-s-client/src/app/gestione-cp/gestione-cp.component.ts
+++ conto-corrente-bancario-s-client/src/app/gestione-cp/gestione-cp.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ContoPrestito } from '../conto-prestito';
 
 @Component({
   selector: 'app-gestione-cp',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestione-cp.component.css']
 })
 export class GestioneCpComponent implements OnInit {
+  contoPrestito: ContoPrestito = new ContoPrestito();
+  showFormChoice: boolean = false;
+  codici: ContoPrestito[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+  new() {
+
+  }
+
+  add() {
+
+  }
+  edit() {
+    this.showFormChoice = true;
+  }
+  delete() {
+
+  }
+
+
 
 }
