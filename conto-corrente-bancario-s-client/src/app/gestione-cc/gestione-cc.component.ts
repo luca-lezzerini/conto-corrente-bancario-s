87c51771ed86a587b6e9b968f0e16c0a52b880ac
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-12 13:52:46 +0100

    Aggiunto metodo aggiungi GestioneCC lato client

--- conto-corrente-bancario-s-client/src/app/gestione-cc/gestione-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/gestione-cc/gestione-cc.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ContoCorrente } from '../conto-corrente';
+import { ContoCorrenteDto } from '../conto-corrente-dto';
+import { ListaContiCorrenteDto } from '../lista-conti-corrente-dto';
 
 @Component({
   selector: 'app-gestione-cc',
@@ -22,6 +24,10 @@ export class GestioneCcComponent implements OnInit {
   }
 
   aggiungi() {
-
+    let dto = new ContoCorrenteDto();
+    dto.contoCorrente = this.contoCorrente;
+    this.http.post<ListaContiCorrenteDto>("http://localhost:8080/aggiungi-cc", dto)
+      .subscribe(r => this.contiCorrente = r.listaContiCorrente);
+    this.contoCorrente = new ContoCorrente();
   }
 }
