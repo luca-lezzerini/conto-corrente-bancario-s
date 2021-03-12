51791210dc5da046cc71484454217ed86d8060b8
ManuelBava <manuelbava1996@gmail.com>
2021-03-12 15:36:08 +0100

    Update ContoCorrenteServiceImpl.java

--- conto-corrente-bancario-server/src/main/java/it/sirfin/contocorrentebancarioserver/serviceImpl/ContoCorrenteServiceImpl.java
+++ conto-corrente-bancario-server/src/main/java/it/sirfin/contocorrentebancarioserver/serviceImpl/ContoCorrenteServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import it.sirfin.contocorrentebancarioserver.service.ContoCorrenteService;
 
 @Service
-public class ConteCorrenteServiceImpl implements ContoCorrenteService {
+public class ContoCorrenteServiceImpl implements ContoCorrenteService {
 
     @Autowired
     ClienteRepository clienteRepository;
