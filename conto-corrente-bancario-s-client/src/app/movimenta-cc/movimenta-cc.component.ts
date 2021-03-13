0f8f90a5c7f322a55a7d927ef3293d78d7411d57
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-03-13 15:23:59 +0100

    Prove sui metodi Movimenta cc

--- conto-corrente-bancario-s-client/src/app/movimenta-cc/movimenta-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/movimenta-cc/movimenta-cc.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
 import { ContoCorrente } from '../conto-corrente';
 import { ListaContiCorrenteDto } from '../dto/lista-conti-corrente-dto';
 import { RicercaContoCorrenteDto } from '../dto/ricerca-conto-corrente-dto';
@@ -13,8 +14,13 @@ export class MovimentaCcComponent implements OnInit {
 
   CodiceConto = "";
   Importo = "";
+  ContoSelezionato = "";
   contiCorrente: ContoCorrente[] = [];
   search = "";
+  url = "http://localhost:8080/";
+  ContoCorrente: ContoCorrente = new ContoCorrente();
+  data: Data;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -22,18 +28,30 @@ export class MovimentaCcComponent implements OnInit {
 
   cerca() {
 
-    let cercaConto = new RicercaContoCorrenteDto();
+    /*let cercaConto = new RicercaContoCorrenteDto();
     cercaConto.codiceEsatto = this.search;
 
-    let ox = this.http.post<ListaContiCorrenteDto>("http://localHost:8080/cerca-movimenta-cc",
+    let ox = this.http.post<ListaContiCorrenteDto>(this.url + "cerca-movimenta-cc",
       cercaConto
     );
 
     ox.subscribe(u => this.contiCorrente = u.listaContiCorrente);
+    */
+
+    let dto = new ContoCorrente();
+    dto.numeroConto = this.CodiceConto;
+    if (this.CodiceConto == "") {
+      console.log("nessun conto selezionato");
+    } else {
+      if (this.CodiceConto == dto.numeroConto) {
+        console.log("C'è l'elemento!");
+      }
+    }
+
   }
 
-  esegui() { 
-    
+  esegui() {
+
   }
 
   seleziona() { }
