1ff4cb84719dc1038aa616c6d54ea21e1e6ba9b3
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-03-13 20:28:42 +0100

    scheletro saldo cc lato client

--- conto-corrente-bancario-s-client/src/app/saldo-cc/saldo-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/saldo-cc/saldo-cc.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { ContoCorrente } from '../conto-corrente';
+import { ListaClientiDto } from '../dto/lista-clienti-dto';
+import { RicercaClienteDto } from '../dto/ricerca-cliente-dto';
 
 @Component({
   selector: 'app-saldo-cc',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saldo-cc.component.css']
 })
 export class SaldoCcComponent implements OnInit {
+  clienti: Cliente[] = [];
+  contiDeposito: ContoCorrente[] = [];
+  ricercaCliente = "";
+  showTableSaldo = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  cerca() {
+    let dto = new RicercaClienteDto();
+    dto.ricercaPerCognome = this.ricercaCliente;
+    this.http.post<ListaClientiDto>("http://localhost:8080/ricerca-c", dto)
+      .subscribe(r => this.clienti = r.listaClienti);
+  }
+
+  seleziona(c: Cliente) {
+    this.showTableSaldo = true;
+  }
+
+  saldo(cc: ContoCorrente) {
+    
+  }
 }
