5b52e0598306150d111d167cb001a909937f4024
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-03-14 19:44:28 +0100

    implementato saldo cp lato client

--- conto-corrente-bancario-s-client/src/app/saldo-cc/saldo-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/saldo-cc/saldo-cc.component.ts
@@ -2,8 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ContoCorrente } from '../conto-corrente';
+import { ClienteDto } from '../dto/cliente-dto';
+import { ContoCorrenteDto } from '../dto/conto-corrente-dto';
+import { ContoDepositoDto } from '../dto/conto-deposito-dto';
 import { ListaClientiDto } from '../dto/lista-clienti-dto';
 import { RicercaClienteDto } from '../dto/ricerca-cliente-dto';
+import { TuttiContiDto } from '../dto/tutti-conti-dto';
 
 @Component({
   selector: 'app-saldo-cc',
@@ -12,7 +16,7 @@ import { RicercaClienteDto } from '../dto/ricerca-cliente-dto';
 })
 export class SaldoCcComponent implements OnInit {
   clienti: Cliente[] = [];
-  contiDeposito: ContoCorrente[] = [];
+  contiCorrente: ContoCorrente[] = [];
   ricercaCliente = "";
   showTableSaldo = false;
 
@@ -30,9 +34,16 @@ export class SaldoCcComponent implements OnInit {
 
   seleziona(c: Cliente) {
     this.showTableSaldo = true;
+    let dto = new ClienteDto();
+    dto.cliente = c;
+    this.http.post<TuttiContiDto>("http://localhost:8080/ricerca-conti-associati-cc", dto)
+      .subscribe(r => this.contiCorrente = r.contiCorrenti);
   }
 
   saldo(cc: ContoCorrente) {
-    
+    let dto = new ContoCorrenteDto();
+    dto.contoCorrente = cc;
+    this.http.post<TuttiContiDto>("http://localhost:8080/visualizza-saldo-cc", dto)
+      .subscribe(r => this.contiCorrente = r.contiCorrenti);
   }
 }
--- conto-corrente-bancario-s-client/src/app/saldo-cp/saldo-cp.component.ts
+++ conto-corrente-bancario-s-client/src/app/saldo-cp/saldo-cp.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { ContoPrestito } from '../conto-prestito';
+import { ClienteDto } from '../dto/cliente-dto';
+import { ContoPrestitoDto } from '../dto/conto-prestito-dto';
+import { ListaClientiDto } from '../dto/lista-clienti-dto';
+import { RicercaClienteDto } from '../dto/ricerca-cliente-dto';
+import { TuttiContiDto } from '../dto/tutti-conti-dto';
 
 @Component({
   selector: 'app-saldo-cp',
@@ -6,10 +14,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saldo-cp.component.css']
 })
 export class SaldoCpComponent implements OnInit {
+  clienti: Cliente[] = [];
+  contiPrestito: ContoPrestito[] = [];
+  ricercaCliente = "";
+  showTableSaldo = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  cerca() {
+    let dto = new RicercaClienteDto();
+    dto.ricercaPerCognome = this.ricercaCliente;
+    this.http.post<ListaClientiDto>("http://localhost:8080/ricerca-cliente-saldo-cp", dto)
+      .subscribe(r => this.clienti = r.listaClienti);
+  }
+
+  seleziona(c: Cliente) {
+    this.showTableSaldo = true;
+    let dto = new ClienteDto();
+    dto.cliente = c;
+    this.http.post<TuttiContiDto>("http://localhost:8080/ricerca-conti-associati-cp", dto)
+      .subscribe(r => this.contiPrestito = r.contiPrestito);
+  }
+
+  saldoCP(cc: ContoPrestito) {
+    let dto = new ContoPrestitoDto();
+    dto.contoPrestito = cc;
+    this.http.post<TuttiContiDto>("http://localhost:8080/visualizza-saldo-cp", dto)
+      .subscribe(r => this.contiPrestito = r.contiPrestito);
+  }
 }
