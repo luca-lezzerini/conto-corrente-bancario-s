a2bc78a1a064e4534c20bff5696092371538ae2b
ManuelBava <manuelbava1996@gmail.com>
2021-03-14 16:00:39 +0100

    implementato scheletro e/c cc

--- conto-corrente-bancario-s-client/src/app/estratto-conto-cc/estratto-conto-cc.component.ts
+++ conto-corrente-bancario-s-client/src/app/estratto-conto-cc/estratto-conto-cc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { ContoCorrente } from '../conto-corrente';
 
 @Component({
   selector: 'app-estratto-conto-cc',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estratto-conto-cc.component.css']
 })
 export class EstrattoContoCcComponent implements OnInit {
-
+  ricercaCliente = "";
+  clienti: Cliente[] = [];
+  contiCorrente: ContoCorrente[] = [];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cercaCliente() {
+
+  }
+  seleziona() {
+
+  }
+  estrattoConto() {
+
+  }
 }
